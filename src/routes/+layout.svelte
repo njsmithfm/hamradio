8ad8ff921eaf07db469828d9053a3d16818bdc76
NJ Smith <134447517+njsmithfm@users.noreply.github.com>
2026-02-10 11:29:37 -0500

    refactor: more troubleshooting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import favicon from '$lib/assets/RochLogo.png';
 
 	let { children } = $props();
+	// If you're using a fallback (i.e. SPA mode) you don't need to prerender all
+// pages by setting this here, but should prerender as many as possible to
+// avoid large performance and SEO impacts
+export const prerender = true;
 </script>
 
 <svelte:head>
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
-export default {
-  preprocess: preprocess(),
-  kit: {
-    adapter: adapter({ pages: 'build', assets: 'build' })
-  }
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
+
+export default config;
